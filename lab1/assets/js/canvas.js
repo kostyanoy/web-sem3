c773b2512c362013803203e064ba6cfeed2be680
Kosty Manuhin <kostymanuhin@gmail.com>
2023-08-13 21:56:37 +0300

    graph draw

---
+++ lab1/assets/js/canvas.js
@@ -0,0 +1,92 @@
+// get canvas
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+let canvasH = canvas.height;
+let canvasW = canvas.width;
+
+let h = canvasH * 3/4;
+let w = canvasW * 3/4;
+
+// draw graph
+// ctx.beginPath();
+drawGraph(ctx);
+
+let radius = 100;
+var startAngle = 0;
+var endAngle = Math.PI / 2; // 90 degrees in radians
+
+var counterclockwise = false;
+
+ctx.save()
+rotate(ctx, Math.PI / 4);
+
+ctx.beginPath();
+ctx.moveTo(w/ 2, h/2); // Move to the center of the circle
+ctx.arc(w/2, h/2, radius, startAngle, endAngle, counterclockwise);
+ctx.lineTo(w/2, h/2); // Connect back to the center
+ctx.fillStyle = "blue";
+ctx.translate(200, 200);
+ctx.rotate(Math.PI / 3);
+ctx.translate(-200, -200)
+ctx.fill();
+//
+// ctx.restore();
+
+// Обработчик события нажатия на холст
+canvas.addEventListener("mousedown", function (event) {
+    var rect = canvas.getBoundingClientRect();
+    var mouseX = event.clientX - rect.left;
+    var mouseY = event.clientY - rect.top;
+
+    // Перевод координат в "координаты холста"
+
+    var canvasX = mouseX - canvas.width / 2;
+    var canvasY = -(mouseY - canvas.height / 2);
+    let a = ctx.getImageData(mouseX, mouseY, 2, 1)
+    console.log(a.data)
+
+    console.log("Clicked at (x, y):", canvasX, canvasY);
+});
+
+function rotate(ctx, rad) {
+    ctx.translate(w / 2, h / 2)
+    ctx.rotate(rad);
+    ctx.translate(-w / 2, -h / 2)
+}
+
+function drawGraph(ctx) {
+    ctx.beginPath();
+    // horizontal line
+    ctx.moveTo(0, canvasH / 2);
+    ctx.lineTo(canvasW, canvasH / 2);
+    // x - arrow
+    ctx.moveTo(canvasW * 0.97, canvasH * 0.53);
+    ctx.lineTo(canvasW, canvasH / 2);
+    ctx.lineTo(canvasW * 0.97, canvasH * 0.47);
+    // vertical line
+    ctx.moveTo(canvasW / 2, 0);
+    ctx.lineTo(canvasW / 2, canvasH);
+    // y - arrow
+    ctx.moveTo(canvasW * 0.47, canvasH * 0.03);
+    ctx.lineTo(canvasW / 2, 0);
+    ctx.lineTo(canvasW * 0.53, canvasH * 0.03);
+
+    ctx.translate(canvasW / 8, canvasH / 8);
+
+    ctx.font = "15px Arial";
+    let labels = [[0, "-R"], [w/4, "-R/2"], [3*w/4, "R/2"], [w, "R"]]
+    // x marks
+    for (let [x, text] of labels) {
+        ctx.moveTo(x, h * 0.48);
+        ctx.lineTo(x, h * 0.52);
+        ctx.fillText(text, x, h * 0.45);
+    }
+    // y marks
+    for (let [y, text] of labels) {
+        ctx.moveTo(w * 0.48, h - y);
+        ctx.lineTo(w * 0.52, h - y);
+        ctx.fillText(text, w * 0.55, h - y);
+    }
+    ctx.stroke();
+}
---
+++ lab1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <title>Web Lab</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+<?php include_once 'includes/header.php'; ?>
+<main>
+<h1>Лабораторная работа #1</h1>
+<p>Content</p>
+
+<div class="canvas-container">
+    <canvas id="myCanvas" width="400" height="400"></canvas>
+</div>
+
+</main>
+
+<script src="assets/js/canvas.js"></script>
+
+<?php include_once 'includes/footer.php'; ?>
+
+</body>
+</html>
