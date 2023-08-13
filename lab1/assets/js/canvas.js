bd2d08b5f937691ef8303f8c7ac1775eabe2e6a5
Kosty Manuhin <kostymanuhin@gmail.com>
2023-08-13 23:27:05 +0300

    draw figures

--- lab1/assets/js/canvas.js
+++ lab1/assets/js/canvas.js
@@ -1,60 +1,19 @@
-// get canvas
-let canvas = document.getElementById("myCanvas");
-let ctx = canvas.getContext("2d");
-
-let canvasH = canvas.height;
-let canvasW = canvas.width;
-
-let h = canvasH * 3/4;
-let w = canvasW * 3/4;
-
-// draw graph
-// ctx.beginPath();
-drawGraph(ctx);
-
-let radius = 100;
-var startAngle = 0;
-var endAngle = Math.PI / 2; // 90 degrees in radians
-
-var counterclockwise = false;
-
-ctx.save()
-rotate(ctx, Math.PI / 4);
-
-ctx.beginPath();
-ctx.moveTo(w/ 2, h/2); // Move to the center of the circle
-ctx.arc(w/2, h/2, radius, startAngle, endAngle, counterclockwise);
-ctx.lineTo(w/2, h/2); // Connect back to the center
-ctx.fillStyle = "blue";
-ctx.translate(200, 200);
-ctx.rotate(Math.PI / 3);
-ctx.translate(-200, -200)
-ctx.fill();
-//
-// ctx.restore();
-
-// Обработчик события нажатия на холст
-canvas.addEventListener("mousedown", function (event) {
-    var rect = canvas.getBoundingClientRect();
-    var mouseX = event.clientX - rect.left;
-    var mouseY = event.clientY - rect.top;
-
-    // Перевод координат в "координаты холста"
-
-    var canvasX = mouseX - canvas.width / 2;
-    var canvasY = -(mouseY - canvas.height / 2);
-    let a = ctx.getImageData(mouseX, mouseY, 2, 1)
-    console.log(a.data)
-
-    console.log("Clicked at (x, y):", canvasX, canvasY);
-});
-
+// rotate canvas
 function rotate(ctx, rad) {
     ctx.translate(w / 2, h / 2)
     ctx.rotate(rad);
     ctx.translate(-w / 2, -h / 2)
 }
 
+// translate to R square
+function shift(back = false) {
+    if (back)
+        ctx.translate(-canvasW / 8, -canvasH / 8);
+    else
+        ctx.translate(canvasW / 8, canvasH / 8);
+}
+
+// draw graph and marks
 function drawGraph(ctx) {
     ctx.beginPath();
     // horizontal line
@@ -72,10 +31,11 @@ function drawGraph(ctx) {
     ctx.lineTo(canvasW / 2, 0);
     ctx.lineTo(canvasW * 0.53, canvasH * 0.03);
 
-    ctx.translate(canvasW / 8, canvasH / 8);
+    shift();
 
     ctx.font = "15px Arial";
-    let labels = [[0, "-R"], [w/4, "-R/2"], [3*w/4, "R/2"], [w, "R"]]
+    ctx.fillStyle = "black";
+    let labels = [[0, "-R"], [w / 4, "-R/2"], [3 * w / 4, "R/2"], [w, "R"]]
     // x marks
     for (let [x, text] of labels) {
         ctx.moveTo(x, h * 0.48);
@@ -89,4 +49,81 @@ function drawGraph(ctx) {
         ctx.fillText(text, w * 0.55, h - y);
     }
     ctx.stroke();
+    shift(true);
 }
+
+function drawRectangle(ctx, width, height) {
+    ctx.beginPath();
+    ctx.moveTo(w / 2, h / 2); // Move to the center of the circle
+    ctx.lineTo(w / 2, h / 2 - height);
+    ctx.lineTo(w / 2 - width, h / 2 - height);
+    ctx.lineTo(w / 2 - width, h / 2);
+    ctx.lineTo(w / 2, h / 2);
+}
+
+function drawRhombus(ctx, width, height) {
+    ctx.beginPath();
+    ctx.moveTo(w / 2, h / 2); // Move to the center of the circle
+    ctx.lineTo(w / 2, h / 2 - height);
+    ctx.lineTo(w / 2 - width, h / 2);
+    ctx.lineTo(w / 2, h / 2);
+}
+
+function drawCircle(ctx, radius) {
+    let startAngle = Math.PI;
+    let endAngle = -Math.PI / 2; // -90 degrees
+    let counterclockwise = false;
+
+    ctx.beginPath();
+    ctx.moveTo(w / 2, h / 2); // Move to the center of the circle
+    ctx.arc(w / 2, h / 2, radius, startAngle, endAngle, counterclockwise);
+    ctx.lineTo(w / 2, h / 2); // Connect back to the center
+}
+
+
+// get canvas
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+let canvasH = canvas.height;
+let canvasW = canvas.width;
+
+let h = canvasH * 3 / 4;
+let w = canvasW * 3 / 4;
+
+// draw figures
+shift();
+drawRectangle(ctx, w / 2, h / 2);
+ctx.fillStyle = "cyan";
+ctx.fill();
+rotate(ctx, Math.PI / 2)
+drawCircle(ctx, h / 2);
+ctx.fillStyle = "cyan";
+ctx.fill();
+rotate(ctx, Math.PI / 2)
+drawRhombus(ctx, w/2, h / 4);
+ctx.fillStyle = "cyan";
+ctx.fill();
+rotate(ctx, Math.PI / 2)
+rotate(ctx, Math.PI / 2)
+
+
+// draw graph
+shift(true);
+drawGraph(ctx);
+
+// Обработчик события нажатия на холст
+canvas.addEventListener("mousedown", function (event) {
+    var rect = canvas.getBoundingClientRect();
+    var mouseX = event.clientX - rect.left;
+    var mouseY = event.clientY - rect.top;
+
+    // Перевод координат в "координаты холста"
+
+    var canvasX = mouseX - canvas.width / 2;
+    var canvasY = -(mouseY - canvas.height / 2);
+    let a = ctx.getImageData(mouseX, mouseY, 2, 1)
+    console.log(a.data)
+
+    console.log("Clicked at (x, y):", canvasX, canvasY);
+});
